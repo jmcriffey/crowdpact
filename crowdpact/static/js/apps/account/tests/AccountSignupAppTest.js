240e0cc4d03146556639945ed330061781f6ebc7
Jeff McRiffey <jeff.mcriffey@ambition.com>
2015-04-11 15:06:57 -0400

    front end plumbing

---
+++ crowdpact/static/js/apps/account/tests/AccountSignupAppTest.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+
+
+describe('AccountSignupApp', () => {
+    it('should run a test', () => {
+        assert.equal(1, 1);
+    });
+});
--- crowdpact/static/js/apps/pact/components/tests/PactAppTest.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-
-
-describe('PactApp', () => {
-    it('should make an assertion', () => {
-        assert.equal(1, 1);
-    });
-});
