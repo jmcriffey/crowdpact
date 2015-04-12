8e6e5075ddfb79e3b9e48bfb4ff807fc1da2a222
Josh Marlow <joshmarlow@gmail.com>
2015-04-12 16:26:26 -0400

    Made pact.index url pattern more restrictive

--- crowdpact/apps/pact/urls.py
+++ crowdpact/apps/pact/urls.py
@@ -5,7 +5,7 @@ from crowdpact.apps.pact.views import PactHomeView
 
 
 urlpatterns = [
-    url(r'', PactHomeView.as_view(), name='pact.index'),
+    url(r'^$', PactHomeView.as_view(), name='pact.index'),
     url(r'^api/details/(?P<id>.+)/?$', PactDetailsView.as_view(), name='pact.api.details'),
     url(r'^api/most-popular/?$', MostPopularPactList.as_view(), name='pact.api.most_popular'),
     url(r'^api/newest/?$', NewestPactList.as_view(), name='pact.api.newest'),
